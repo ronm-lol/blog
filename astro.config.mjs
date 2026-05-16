9f5e3a0cef71c0c583643d8d91445b3a19b6d897
rm <ronm@omg.lol>
2026-05-16 11:26:15 -0400

    lets try synthwave

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      theme: 'synthwave-84',
       langs: [],
       wrap: true,
     },
