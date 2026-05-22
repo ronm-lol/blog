3e6491a82ffd07769773aa8a3ffb861c06683202
rm <ronm@omg.lol>
2026-05-21 21:02:42 -0400

    remove zwsp plugin, it breaks word wrapping

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { remarkZwsp } from './src/plugins/remark-zwsp.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ronm.lol',
   base: '/',
   markdown: {
-    remarkPlugins: [remarkZwsp],
+    remarkPlugins: [],
     shikiConfig: {
       theme: 'synthwave-84',
       langs: [],
